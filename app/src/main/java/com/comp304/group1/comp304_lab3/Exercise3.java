8ced124e01c143c277a46f523a8c278b8e4fff5e
Ilmir Taychinov <riml@railgun-games.com>
2016-10-20 17:34:01 -0400

    Jacky: fixed moon animation

--- app/src/main/java/com/comp304/group1/comp304_lab3/Exercise3.java
+++ app/src/main/java/com/comp304/group1/comp304_lab3/Exercise3.java
@@ -2,6 +2,7 @@ package com.comp304.group1.comp304_lab3;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -12,15 +13,21 @@ import android.widget.ImageView;
 
 public class Exercise3 extends AppCompatActivity {
     private ImageView moonImg;
+    private Animation orbit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_exercise3);
         moonImg = (ImageView) findViewById(R.id.moon);
-        Animation orbit = AnimationUtils.loadAnimation(this, R.anim.orbit);
+
+        orbit = AnimationUtils.loadAnimation(this, R.anim.orbit);
+        orbit.setRepeatCount(Animation.INFINITE);
+        Log.d("123", "-------------------------------------------");
+        Log.d("MoonImg", "J " + moonImg.toString());
+
         moonImg.startAnimation(orbit);
     }
+
 
 }
